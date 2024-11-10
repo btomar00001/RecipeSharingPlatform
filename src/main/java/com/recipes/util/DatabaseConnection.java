a828654f2be47fc6b4203429cf8157b6fead20f4
Bhuvaneshwar Pratap <devashishshukla@DEVASHISHs-MacBook-Air.local>
2024-11-10 16:23:53 +0530

    Add Review-1 : Project setup, database-schema, JDBC setup, DAO classes

---
+++ src/main/java/com/recipes/util/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.recipes.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/recipes_db";
+    private static final String USER = "root"; // replace with your MySQL username
+    private static final String PASSWORD = "Ptwmjgad12@"; // replace with your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
